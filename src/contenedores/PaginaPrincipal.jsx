ad23a8ef4c124e7c742a1a55807c87f7fcadc51b
MateoMor <mateomorales5558@gmail.com>
2024-06-27 13:48:20 -0500

    Funcionalidad de pedir datos a api implementada en home

--- src/contenedores/PaginaPrincipal.jsx
+++ src/contenedores/PaginaPrincipal.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/PaginaPrincipal.css'
 import TarjetaRecomendacion from './TarjetaRecomendacion'
+import getSearch from '../services/getMovies'
 
 function PaginaPrincipal () {
+  const [mostPopularMovies, setMostPopularMovies] = React.useState([])
+  const [mostPopularTvShows, setMostPopularTvShows] = useState([])
+
+  useEffect(() => {
+    const search = async (queryLine) => {
+      try {
+        const { data } = await getSearch('tv', {
+          query: queryLine,
+          page: 1
+        })
+        const movies = data.results.slice(0, 5) // Limitar a los primeros 5 elementos
+
+        console.log(movies)
+        setMostPopularMovies(movies)
+      } catch (error) {
+        console.log('Error', error)
+      }
+    }
+
+    // Llamada a la función de búsqueda con una query inicial
+    search('popular')
+  }, [])
+
   return (
     <div className='contenedor-principal'>
       <h1>Peliculas populares</h1>
+
+      {mostPopularMovies.map((movie) => (
+        <TarjetaRecomendacion
+          key={movie.id} // Añadir una clave única para cada elemento
+          title={movie.title || movie.name || 'Un título'}
+          rating={movie.vote_average || 5}
+          imgPath={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} // Solicitar la imagen correcta
+          alt={movie.title || movie.name || 'Película'}
+        />
+      ))}
+
       <div className='principal-contenedor-recomendaciones'>
         <TarjetaRecomendacion title='Un titulo' rating={5} imgPath='https://picsum.photos/200/300' alt='Pelicula 1' />
         <TarjetaRecomendacion title='Un titulo' rating={4} imgPath='https://picsum.photos/200/300' alt='Pelicula 2' />
