dbd9c837720005b5c11f8990ca5be76468ffe763
MateoMor <mateomorales5558@gmail.com>
2024-06-26 20:33:30 -0500

    Reparación del contexto

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import { useState } from 'react'
 import './App.css'
-import { GlobalProvider } from './context/GlobalState'
+import { useGlobalState } from './context/GlobalState'
 
 function App () {
-  const [estadoPrueba, setEstadoPrueba] = useState(true) /* Los estados se importan del contexto */
+  const { estadoPrueba, setEstadoPrueba } = useGlobalState() /* Los estados se importan del contexto */
 
   return (
-    <GlobalProvider>
-      <div>
-        <h1>{estadoPrueba}</h1>
-        <button onClick={() => {
-          setEstadoPrueba(!estadoPrueba)
-          console.log(estadoPrueba)
-        }}
-        >Cambia estado
-        </button>
-      </div>
-    </GlobalProvider>
+
+    <div>
+      <h1>{estadoPrueba ? 'Estoy activo' : 'Estoy inactivo'}</h1>
+      <button onClick={() => {
+        setEstadoPrueba(!estadoPrueba)
+        console.log(estadoPrueba)
+      }}
+      >Cambia estado
+      </button>
+    </div>
+
   )
 }
 
