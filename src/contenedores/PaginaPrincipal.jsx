d53bafdc1edc0c24e1e2490c555ec2a768e5e4cd
MateoMor <mateomorales5558@gmail.com>
2024-06-27 15:04:04 -0500

    Links añadidos a las tarjetas

--- src/contenedores/PaginaPrincipal.jsx
+++ src/contenedores/PaginaPrincipal.jsx
@@ -14,7 +14,7 @@ function PaginaPrincipal () {
           query: queryLine,
           page: 1
         })
-        const movies = data.results.slice(0, 5) // Limitar a los primeros 5 elementos
+        const movies = data.results.slice(0, 5)
 
         console.log(movies)
         setMostPopularMovies(movies)
@@ -23,7 +23,6 @@ function PaginaPrincipal () {
       }
     }
 
-    // Llamada a la función de búsqueda con una query inicial
     search('popular')
   }, [])
 
@@ -31,22 +30,17 @@ function PaginaPrincipal () {
     <div className='contenedor-principal'>
       <h1>Peliculas populares</h1>
 
-      {mostPopularMovies.map((movie) => (
-        <TarjetaRecomendacion
-          key={movie.id} // Añadir una clave única para cada elemento
-          title={movie.title || movie.name || 'Un título'}
-          rating={movie.vote_average || 5}
-          imgPath={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} // Solicitar la imagen correcta
-          alt={movie.title || movie.name || 'Película'}
-        />
-      ))}
-
       <div className='principal-contenedor-recomendaciones'>
-        <TarjetaRecomendacion title='Un titulo' rating={5} imgPath='https://picsum.photos/200/300' alt='Pelicula 1' />
-        <TarjetaRecomendacion title='Un titulo' rating={4} imgPath='https://picsum.photos/200/300' alt='Pelicula 2' />
-        <TarjetaRecomendacion title='Un titulo' rating={3} imgPath='https://picsum.photos/200/300' alt='Pelicula 3' />
-        <TarjetaRecomendacion title='Un titulo' rating={2} imgPath='https://picsum.photos/200/300' alt='Pelicula 4' />
-        <TarjetaRecomendacion title='Un titulo' rating={1} imgPath='https://picsum.photos/200/300' alt='Pelicula 5' />
+        {mostPopularMovies.map((movie) => (
+          <TarjetaRecomendacion
+            key={movie.id} // Añadir una clave única para cada elemento
+            title={movie.title || movie.name || 'Un título'}
+            rating={movie.vote_average || 5}
+            imgPath={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} // Solicitar la imagen correcta
+            alt={movie.title || movie.name || 'Película'}
+          />
+        ))}
+
       </div>
 
       <h1>Series mas populares</h1>
