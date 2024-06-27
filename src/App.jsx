3c432f0ee1989839e3889cf4fa2a2f56b5b0b987
MateoMor <mateomorales5558@gmail.com>
2024-06-26 21:21:49 -0500

    React router configurated

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
+import { Link, Route, Routes } from 'react-router-dom'
 import './App.css'
-import { useGlobalState } from './context/GlobalState'
+/* import { useGlobalState } from './context/GlobalState' */
 
 function App () {
-  const { estadoPrueba, setEstadoPrueba } = useGlobalState() /* Los estados se importan del contexto */
+  /* const { estadoPrueba, setEstadoPrueba } = useGlobalState()  *//* Los estados se importan del contexto */
 
   return (
 
     <div>
-      <h1>{estadoPrueba ? 'Estoy activo' : 'Estoy inactivo'}</h1>
-      <button onClick={() => {
-        setEstadoPrueba(!estadoPrueba)
-        console.log(estadoPrueba)
-      }}
-      >Cambia estado
-      </button>
+      <Link to='/'>Home</Link>
+      <Link to='/about'>About</Link>
+      <Routes>
+        <Route path='/' element={<h1>Home</h1>}>Home</Route>
+        <Route path='/about' element={<h1>About</h1>}>About</Route>
+      </Routes>
     </div>
 
   )
