41762421c640efadaaf3e8d01988b3d5cced9068
MateoMor <mateomorales5558@gmail.com>
2024-06-26 19:57:54 -0500

    Hook de Context añadido

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '../../../../../../vite.svg'
 import './App.css'
+import { GlobalProvider } from './context/GlobalState'
 
 function App () {
-  const [count, setCount] = useState(0)
+  const [estadoPrueba, setEstadoPrueba] = useState(true) /* Los estados se importan del contexto */
 
   return (
-    <>
+    <GlobalProvider>
       <div>
-        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <h1>{estadoPrueba}</h1>
+        <button onClick={() => {
+          setEstadoPrueba(!estadoPrueba)
+          console.log(estadoPrueba)
+        }}
+        >Cambia estado
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </GlobalProvider>
   )
 }
 
